9c1bca8a14f641b5d2e9af661adc1a8a9580f2f2
rhobbikhaidir <rhobbie09@gmail.com>
2024-12-13 09:55:50 +0700

    add service detail tours

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const fs = require('fs')
 const port = 3000;
 const app = express();
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+app.use(express.urlencoded({ extended: true }));
+// This is required to handle urlencoded data
+app.use(express.json()); 
+// This to handle jsoan data coming from requests mainly post1
 
 // app.get('/', (req, res) => {
 //   res.status(200).send('Hello From server side anjay');
@@ -16,7 +22,7 @@ const tours = JSON.parse(
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status: 'success anjay',
+    status: 'success',
     results: tours.length,
     data: {
       tours,
@@ -24,6 +30,43 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body)
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+            tour: newTour,
+        },
+      }); 
+    }
+  );
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+
+
+  console.log(req.params, '***ini params')
+  const newId = req.params.id * 1
+
+  const tour = tours.find((el) => el?.id === newId )
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);

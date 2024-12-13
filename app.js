d1bbc707743079615bff3964e34263321a4d310b
rhobbikhaidir <rhobbie09@gmail.com>
2024-12-13 11:14:03 +0700

    add service patch and delete

--- app.js
+++ app.js
@@ -72,6 +72,39 @@ app.get('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  
+  if(req.params.id * 1 > tours?.length) {
+    return res.status(404).json({
+        status: 'fail',
+        message: 'Invalid ID'
+      })
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: '<Updated tour here...> '
+      }
+    })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  
+  if(req.params.id * 1 > tours?.length) {
+    return res.status(404).json({
+        status: 'fail',
+        message: 'Invalid ID'
+      })
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: 204
+    })
+})
+
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
